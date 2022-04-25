110f9b712f14443d7dcb94bfeffe20ed25623fa7
桑浩翔 <97347493+Haoxiang-SANG@users.noreply.github.com>
2022-04-25 16:11:10 +0800

    Create yjmclass.py

---
+++ friends/yjmclass.py
@@ -0,0 +1,43 @@
+class Pet:
+    def _init_(self):
+        self.__name=name
+        self.__age=age
+        self.sound=sound
+    def show(self):
+        print('Name=',self.name,',Age=',self.age)
+    def makeSound(self): 
+        print('MakeSound=',self.sound)
+        
+class Dog(Pet):
+    def _init_(self,breed):
+        self.__breed=breed
+
+D=Pet()
+D.name='lucky'
+D.age=6
+D.sound='wang...wang...wang...'
+breed='哈士奇'
+D.show()
+D.makeSound()
+
+class Pet:
+    def _init_(self):
+        self.__name=name
+        self.__age=age
+        self.sound=sound
+    def show(self):
+        print('Name=',self.name,',Age=',self.age)
+    def makeSound(self): 
+        print('MakeSound=',self.sound)
+        
+class Dog(Pet):
+    def _init_(self,breed):
+        self.__breed=breed
+
+D=Pet()
+D.name='lucky'
+D.age=6
+D.sound='wang...wang...wang...'
+breed='哈士奇'
+D.show()
+D.makeSound()
